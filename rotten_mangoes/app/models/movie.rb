2c30cd58b607aefea1b5ace830306ae88f23934a
vagrant <natsanjuro@gmail.com>
2015-10-20 16:57:30 +0000

    first day changes

---
+++ rotten_mangoes/app/models/movie.rb
@@ -0,0 +1,41 @@
+class Movie < ActiveRecord::Base
+
+  has_many :reviews
+
+  validates :title,
+    presence: true
+
+  validates :director,
+    presence: true
+
+  validates :runtime_in_minutes,
+    numericality: { only_integer: true }
+
+  validates :description,
+    presence: true
+
+  validates :poster_image_url,
+    presence: true
+
+  validates :release_date,
+    presence: true
+
+  validate :release_date_is_in_the_future
+
+  def review_average
+    if reviews.size == 0
+      review_average = 0
+    else  
+      reviews.sum(:rating_out_of_ten)/reviews.size
+    end
+  end
+
+  protected
+
+  def release_date_is_in_the_future
+    if release_date.present?
+      errors.add(:release_date, "should probably be in the future") if release_date < Date.today
+    end
+  end
+
+end
